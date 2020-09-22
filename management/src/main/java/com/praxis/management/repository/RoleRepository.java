89c0f0c3028e58edc842bd56339eeefe0a7b4381
Prisyaji <aji18suroso@gmail.com>
2020-09-22 16:25:02 +0700

    Update JWT

---
+++ management/src/main/java/com/praxis/management/repository/RoleRepository.java
@@ -0,0 +1,12 @@
+package com.praxis.management.repository;
+
+import com.praxis.management.model.Role;
+import com.praxis.management.model.RoleName;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.Optional;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Optional<Role> findByName(RoleName roleName);
+}
