b7a29f379556b5af320c037d3720b1f9372e7e06
Walid nurudin <walidnurudin47@gmail.com>
2020-09-22 23:15:44 +0700

    register 30%

--- frontend/src/components/form-register.vue
+++ frontend/src/components/form-register.vue
@@ -1,79 +1,110 @@
 <template>
   <div>
     <v-container>
-      <form @submit="checkForm" novalidate="true">
+      <v-form ref="form" lazy-validation>
         <v-row>
           <v-col cols="12" sm="6">
-            <v-text-field v-model="name" label="Name" required></v-text-field>
+            <v-text-field v-model="data.name" label="Name" :rules="nameVal" required></v-text-field>
           </v-col>
           <v-col cols="12" sm="6">
-            <v-select v-model="program" :items="kelas" label="Program" required></v-select>
-          </v-col>
-        </v-row>
-
-        <v-row>
-          <v-col cols="12" sm="6">
-            <v-text-field v-model="email" label="Email" required></v-text-field>
-          </v-col>
-          <v-col cols="12" sm="6">
-            <v-text-field v-model="tempatLahir" label="Tempat lahir" required></v-text-field>
+            <v-select
+              v-model="data.program"
+              :items="kelas"
+              :rules="programVal"
+              label="Program"
+              required
+            ></v-select>
           </v-col>
         </v-row>
 
         <v-row>
           <v-col cols="12" sm="6">
-            <v-text-field v-model="tanggalLahir" label="Tanggal lahir" required></v-text-field>
+            <v-text-field v-model="data.email" :rules="emailVal" label="Email" required></v-text-field>
           </v-col>
           <v-col cols="12" sm="6">
-            <v-text-field v-model="nomorHP" label="Nomor HP" required></v-text-field>
+            <v-text-field
+              v-model="data.tempatLahir"
+              :rules="tempatlahirVal"
+              label="Tempat lahir"
+              required
+            ></v-text-field>
+          </v-col>
+        </v-row>
+
+        <v-row>
+          <v-col cols="12" sm="6">
+            <v-text-field
+              v-model="data.tanggalLahir"
+              :rules="tanggallahirVal"
+              label="Tanggal lahir"
+              required
+            ></v-text-field>
+          </v-col>
+          <v-col cols="12" sm="6">
+            <v-text-field v-model="data.nomorHP" :rules="nomorhpVal" label="Nomor HP" required></v-text-field>
           </v-col>
         </v-row>
 
         <v-row>
           <v-col>
-            <v-text-field v-model="kotaAsal" label="Kota asal" required></v-text-field>
+            <v-text-field v-model="data.kotaAsal" :rules="kotaasalVal" label="Kota asal" required></v-text-field>
           </v-col>
         </v-row>
 
         <v-row>
           <v-col>
-            <v-textarea v-model="alamat" autocomplete="alamat" label="Alamat"></v-textarea>
+            <v-textarea
+              v-model="data.alamat"
+              :rules="alamatVal"
+              autocomplete="alamat"
+              label="Alamat"
+            ></v-textarea>
           </v-col>
         </v-row>
 
         <v-row>
           <v-col>
-            <v-select v-model="pendidikan" :items="pendidikanArr" label="Pendidikan" required></v-select>
+            <v-select
+              v-model="data.pendidikan"
+              :rules="pendidikanVal"
+              :items="pendidikanArr"
+              label="Pendidikan"
+              required
+            ></v-select>
           </v-col>
         </v-row>
 
         <v-row>
           <v-col>
-            <v-text-field v-model="namaKampus" label="Nama kampus" required></v-text-field>
+            <v-text-field v-model="data.namaKampus" :rules="namakampusVal" label="Nama kampus" required></v-text-field>
           </v-col>
         </v-row>
 
         <v-row>
           <v-col>
-            <v-select v-model="semester" :items="semesterArr" label="Semester" required></v-select>
+            <v-select
+              v-model="data.semester"
+              :rules="semesterVal"
+              :items="semesterArr"
+              label="Semester"
+              required
+            ></v-select>
           </v-col>
         </v-row>
 
         <v-row>
           <v-col>
-            <v-textarea v-model="alamatKampus" autocomplete="alamat kampus" label="Alamat kampus"></v-textarea>
+            <v-textarea
+              v-model="data.alamatKampus"
+              :rules="alamatkampusVal"
+              autocomplete="alamat kampus"
+              label="Alamat kampus"
+            ></v-textarea>
           </v-col>
         </v-row>
 
-        <v-btn class="mr-4" type="submit">submit</v-btn>
-
-        <div v-if="errors.length">
-          <h1>is required:</h1>
-          <div v-for="(item, index) in errors" :key="index">
-            <h1 class="red--text">{{item}}</h1>
-          </div>
-        </div>
-      </form>
+        <v-btn class="mr-4" @click="submit">submit</v-btn>
+      </v-form>
     </v-container>
   </div>
 </template>
@@ -82,41 +113,49 @@
 export default {
   data: () => {
     return {
-      errors: [],
+      // select
       kelas: ["frontend", "backend"],
       pendidikanArr: ["S1", "S2", "S3", "Lainnya"],
       semesterArr: ["semester 7", "semester 8"],
-      name: null,
-      email: null,
-      program: null,
-      tempatLahir: null,
-      tanggalLahir: null,
-      nomorHP: null,
-      kotaAsal: null,
-      alamat: null,
-      pendidikan: null,
-      namaKampus: null,
-      semester: null,
-      alamatKampus: null
+
+      // data
+      data: {
+        name: "",
+        email: null,
+        program: null,
+        tempatLahir: null,
+        tanggalLahir: null,
+        nomorHP: null,
+        kotaAsal: null,
+        alamat: null,
+        pendidikan: null,
+        namaKampus: null,
+        semester: null,
+        alamatKampus: null
+      },
+
+      // validate
+      nameVal: [v => v.length >= 3 || "name length is 3 character"],
+      programVal: [v => !!v || "required"],
+      emailVal: [
+        v => !!v || "E-mail is required",
+        v => /.+@.+\..+/.test(v) || "E-mail must be valid"
+      ],
+      tempatlahirVal: [v => !!v || "required"],
+      kotaasalVal: [v => !!v || "required"],
+      tanggallahirVal: [v => !!v || "required"],
+      nomorhpVal: [v => !!v || "required"],
+      alamatVal: [v => !!v || "required"],
+      pendidikanVal: [v => !!v || "required"],
+      namakampusVal: [v => !!v || "required"],
+      semesterVal: [v => !!v || "required"],
+      alamatkampusVal: [v => !!v || "required"]
     };
   },
   methods: {
-    checkForm: function(e) {
-      this.errors = [];
-
-      if (!this.name) this.errors.push("Name required.");
-
-      if (!this.email) {
-        this.errors.push("Email required.");
-      } else if (!this.validEmail(this.email)) {
-        this.errors.push("Valid email required.");
-      }
-      if (!this.errors.length) return true;
-      e.preventDefault();
-    },
-    validEmail: function(email) {
-      var re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-      return re.test(email);
+    submit: function() {
+      if (this.$refs.form.validate()) console.log(this.data);
+      else alert("error cuy");
     }
   }
 };
