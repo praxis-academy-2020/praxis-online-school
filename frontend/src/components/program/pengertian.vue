1b508bbfc6766ff193f6cfaf4a646fa058bf3f15
97arkham <97arkham>
2020-10-28 18:35:48 +0700

    jaja

---
+++ frontend/src/components/program/pengertian.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        pengertian
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+<style  scoped>
+
+</style>
--- frontend/src/views/Program.vue
+++ frontend/src/views/Program.vue
@@ -3,7 +3,11 @@
     <navbar />
     <div class="mt-15">
         <Hero />
+        <Pengertian />
+        <Silabus />
+        <Ajakan />
       <h1>Program {{ this.$route.params.name }}</h1>
+        
     </div>
     <foot />
   </div>
@@ -13,12 +17,18 @@
 import navbar from "@/components/navbar/navbar-home.vue";
 import Hero from "@/components/program/hero.vue"
 import foot from "@/components/footer/footer-home.vue";
+import Pengertian from"@/components/program/pengertian.vue";
+import Silabus from "@/components/program/silabus.vue";
+import Ajakan from "@/components/program/ajakan.vue";
 
 export default {
   components: {
     foot,
     navbar,
-    Hero
+    Hero,
+    Pengertian,
+    Silabus,
+    Ajakan,
   },
 };
 </script>
