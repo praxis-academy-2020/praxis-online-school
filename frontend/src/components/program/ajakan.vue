6242e222cc85d82352b22f93e37098466cd66ff4
Walidnurudin <walidnurudin47@gmail.com>
2020-10-28 22:36:57 +0700

    section ajakan in program

--- frontend/src/components/program/ajakan.vue
+++ frontend/src/components/program/ajakan.vue
@@ -1,11 +1,16 @@
 <template>
-    <div>Ajakan</div>
-</template>
-<script>
-export default {
-    
-}
-</script>
-<style scoped>
+  <div
+    class="grey lighten-4 d-flex justify-center text-center"
+    style="padding: 100px 0"
+  >
+    <div>
+      <div class="mb-10 text-h3 font-weight-medium">
+        Siap ubah masa depanmu?
+      </div>
 
-</style>
+      <v-btn to="/register/syarat" large color="#63D0FF" class="white--text">
+        daftar
+      </v-btn>
+    </div>
+  </div>
+</template>
