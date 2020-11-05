f9e4769b3e288bdc6dd087d32ba4771e0bc8fba1
97arkham <97arkham>
2020-11-05 20:05:12 +0700

    updt home karya dan page karya

--- frontend/src/components/home/fasilitas.vue
+++ frontend/src/components/home/fasilitas.vue
@@ -3,7 +3,7 @@
     <v-container>
       <v-row>
         <v-col class="d-flex justify-center mb-10">
-          <div class="mb-10 text-h4 text-sm-h3 font-weight-medium">
+          <div class="mb-10 text-h4 text-sm-h3 font-weight-medium primary--text">
             Program Praxis Academy
           </div>
         </v-col>
@@ -11,7 +11,7 @@
       <v-row class="d-flex justify-space-around">
         <v-col class="pa-4" cols="12" sm="4">
           <v-icon large color="black" class="mr-2">mdi-language-java</v-icon>
-          <span><strong>Fullstack Developer</strong></span>
+          <span class="primary--text"><strong>Fullstack Developer</strong></span>
           <p class="mt-3">
             mengembangkan interface website menggunakan: javascript, VueJs,
             Vuetify
@@ -19,7 +19,7 @@
         </v-col>
         <v-col class="pa-4" cols="12" sm="4">
           <v-icon large color="black" class="mr-2">mdi-language-python</v-icon>
-          <span><strong>Python Developer</strong></span>
+          <span class="primary--text"><strong>Python Developer</strong></span>
           <p class="mt-3">
             mengembangkan interface website menggunakan: javascript, VueJs,
             Vuetify
@@ -29,7 +29,7 @@
           <v-icon large color="black" class="mr-2"
             >mdi-language-javascript</v-icon
           >
-          <span><strong>Frontend Developer</strong></span>
+          <span class="primary--text"><strong>Frontend Developer</strong></span>
           <p class="mt-3">
             mengembangkan interface website menggunakan: javascript, VueJs,
             Vuetify
@@ -37,7 +37,7 @@
         </v-col>
         <v-col class="pa-4" cols="12" sm="4">
           <v-icon large color="black" class="mr-2">mdi-react</v-icon>
-          <span><strong>Mobile Developer</strong></span>
+          <span class="primary--text"><strong>Mobile Developer</strong></span>
           <p class="mt-3">
             mengembangkan interface website menggunakan: javascript, VueJs,
             Vuetify
@@ -45,7 +45,7 @@
         </v-col>
         <v-col class="pa-4" cols="12" sm="4">
           <v-icon large color="black" class="mr-2">mdi-nodejs</v-icon>
-          <span><strong>Nodejs Programming</strong></span>
+          <span class="primary--text"><strong>Nodejs Programming</strong></span>
           <p class="mt-3">
             mengembangkan interface website menggunakan: javascript, VueJs,
             Vuetify
--- frontend/src/views/Creation.vue
+++ frontend/src/views/Creation.vue
@@ -1,7 +1,27 @@
 <template>
   <div>
+
     <navbarHome :isInverted="false" />
-    <cardssss class="mt-15" />
+
+    <v-container>
+               <div class="text-h4 text-sm-h3 font-weight-medium" style="margin-top:100px; text-align:center;">
+            Karya Praxis Academy
+          </div>
+    <v-row>
+      <v-col cols='6'>
+    <cardssss src='https://www.youtube.com/embed/UiPv8GGt_Ik' class="mt-15" hrf='http://salamdoc.id/'/>
+    </v-col>
+    <v-col cols='6'>
+    <cardssss src='https://www.youtube.com/embed/UiPv8GGt_Ik' class="mt-15" />
+    </v-col>
+    <v-col cols='6'>
+    <cardssss src='https://www.youtube.com/embed/UiPv8GGt_Ik' class="mt-15" />
+    </v-col>
+    <v-col cols='6'>
+    <cardssss src='https://www.youtube.com/embed/UiPv8GGt_Ik' class="mt-15" />
+    </v-col>
+    </v-row>
+    </v-container>
     <foot />
   </div>
 </template>
@@ -9,7 +29,7 @@
 <script>
 import navbarHome from "@/components/navbar/navbar-home.vue";
 import foot from "@/components/footer/footer-home.vue";
-import cardssss from "@/components/karya/card.vue";
+import cardssss from "@/components/karya/karyo.vue";
 
 export default {
   name: "Home",
